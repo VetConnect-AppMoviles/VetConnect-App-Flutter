505f44fe36a015d20689075cffe652ba7a3f348e
RodrLH <u202212338@upc.edu.pe>
2024-10-24 17:44:18 -0500

    feat: edit main.dart

--- app_vetconnect_flutter/lib/main.dart
+++ app_vetconnect_flutter/lib/main.dart
@@ -2,16 +2,12 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/payment_screen.dart';
-//Victor
 import 'screens/veterinary_config.dart';
 import 'screens/home_veterinary_screen.dart';
-import 'screens/home_veterinary_screen3.dart';
 import 'screens/home_veterinary_screen2.dart';
-//Harold
 import 'screens/calendar_screen.dart';
 import 'screens/terminos_screen.dart';
 import 'screens/my_home_page.dart';
-//Brayan
 import 'screens/perfil_screen.dart';
 import 'screens/config_perfil_empresa.dart';
 import 'screens/add_service_screen.dart';
@@ -31,20 +27,15 @@ class VetConnectApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/',
       routes: {
-        //Rodrigo
         '/': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
         '/payment': (context) => const PaymentScreen(),
-        //Victor
         '/home': (context) => HomeVeterinaryScreen(),
         '/veterinary': (context) =>const  VeterinaryConfig(),
         '/home2': (context) => const HomeVeterinaryScreen2(),
-        '/home3': (context) => const HomeVeterinaryScreen3(),
-        //Harold
         '/calendar': (context) => const CalendarView(),
         '/terminos': (context) => const TerminosPage(),
         '/myhome': (context) => const MyHomePage(),
-        //Brayan
         '/perfil': (context) => const PerfilScreen(),
         '/config': (context) => const ConfigPerfilEmpresaApp(),
         '/addservice': (context) => const AddServiceScreen(),
