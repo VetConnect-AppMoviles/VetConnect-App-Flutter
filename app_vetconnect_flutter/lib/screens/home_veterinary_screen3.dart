5d716f4de763f09ae77cfb04fe4f4f68e12a0ff0
vrokerev <vmeneses782000@gmail.com>
2024-10-24 11:53:27 -0500

    feat: add home_veterinary_screen3.dart

---
+++ app_vetconnect_flutter/lib/screens/home_veterinary_screen3.dart
@@ -0,0 +1,34 @@
+import 'package:app_vetconnect_flutter/extra/home_veterinary_appbar.dart';
+import 'package:app_vetconnect_flutter/extra/service_information_card.dart';
+import 'package:app_vetconnect_flutter/extra/vet_clinic_card.dart';
+import 'package:flutter/material.dart';
+// Importar tus componentes
+
+class HomeVeterinaryScreen3 extends StatefulWidget {
+  const HomeVeterinaryScreen3({super.key});
+
+  @override
+  State<HomeVeterinaryScreen3> createState() => _HomeVeterinaryScreen3State();
+}
+
+class _HomeVeterinaryScreen3State extends State<HomeVeterinaryScreen3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: HomeVeterinaryAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              VetClinicCard(), // Componente de la tarjeta de la clínica veterinaria
+              SizedBox(height: 16), // Espacio entre los componentes
+              ServiceInformationCard(), // Componente de la información del servicio
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
