6f6a4e23a7e046eb18828658c8c018b5bca45cc7
vrokerev <vmeneses782000@gmail.com>
2024-10-24 11:53:22 -0500

    feat: add home_veterinary_screen2.dart

---
+++ app_vetconnect_flutter/lib/screens/home_veterinary_screen2.dart
@@ -0,0 +1,43 @@
+import 'package:app_vetconnect_flutter/extra/home_veterinary_appbar.dart';
+import 'package:app_vetconnect_flutter/extra/home_veterinary_buttons.dart';
+import 'package:app_vetconnect_flutter/extra/vet_clinic_card.dart';
+import 'package:app_vetconnect_flutter/extra/veterinary_description.dart';
+import 'package:app_vetconnect_flutter/extra/veterinary_opiniones.dart';
+import 'package:app_vetconnect_flutter/extra/veterinary_services.dart';
+import 'package:flutter/material.dart';
+
+// Asegúrate de importar los widgets que creaste
+
+class HomeVeterinaryScreen2 extends StatefulWidget {
+  const HomeVeterinaryScreen2({super.key});
+
+  @override
+  State<HomeVeterinaryScreen2> createState() => _HomeVeterinaryScreen2State();
+}
+
+class _HomeVeterinaryScreen2State extends State<HomeVeterinaryScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: HomeVeterinaryAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(
+              16.0), // Opcional, para un poco de espacio alrededor
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              CustomButtons(),
+              VetClinicCard(),
+              VeterinaryDescription(), // Llamas al widget de descripción
+              SizedBox(height: 16), // Espacio entre secciones
+              VeterinaryServices(), // Llamas al widget de servicios
+              SizedBox(height: 16), // Espacio entre secciones
+              VeterinaryOpinions(), // Llamas al widget de opiniones
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
