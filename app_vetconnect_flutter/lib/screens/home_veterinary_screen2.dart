f76a98dd29089503a6ae2dddb1abebf06d24c0c4
Rodrigo Salvador <u202213646@upc.edu.pe>
2024-10-24 19:06:40 -0500

    feat: update home_veterinary_screen2.dart

--- app_vetconnect_flutter/lib/screens/home_veterinary_screen2.dart
+++ app_vetconnect_flutter/lib/screens/home_veterinary_screen2.dart
@@ -1,12 +1,12 @@
-import 'package:app_vetconnect_flutter/widgets/home_veterinary_appbar.dart';
-import 'package:app_vetconnect_flutter/widgets/home_veterinary_buttons.dart';
-import 'package:app_vetconnect_flutter/widgets/vet_clinic_card.dart';
-import 'package:app_vetconnect_flutter/widgets/veterinary_description.dart';
-import 'package:app_vetconnect_flutter/widgets/veterinary_opiniones.dart';
-import 'package:app_vetconnect_flutter/widgets/veterinary_services.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
-// Asegúrate de importar los widgets que creaste
+import 'package:http/http.dart' as http;
+import '../widgets/home_veterinary_appbar.dart';
+import '../widgets/home_veterinary_buttons.dart';
+import '../widgets/vet_clinic_card.dart';
+import '../widgets/veterinary_description.dart';
+import '../widgets/veterinary_opiniones.dart';
+import '../widgets/veterinary_services.dart';
 
 class HomeVeterinaryScreen2 extends StatefulWidget {
   const HomeVeterinaryScreen2({super.key});
@@ -16,24 +16,50 @@ class HomeVeterinaryScreen2 extends StatefulWidget {
 }
 
 class _HomeVeterinaryScreen2State extends State<HomeVeterinaryScreen2> {
+  Map<String, dynamic>? vetCenter;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchVetCenter();
+  }
+
+  Future<void> fetchVetCenter() async {
+    final response = await http.get(Uri.parse('https://my-json-server.typicode.com/RodrLH/vetconnect-jsonserver/vet-centers/1'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        vetCenter = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load vet center');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeVeterinaryAppBar(),
-      body: SingleChildScrollView(
+      body: vetCenter == null
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(
-              16.0), // Opcional, para un poco de espacio alrededor
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              CustomButtons(),
-              VetClinicCard(),
-              VeterinaryDescription(), // Llamas al widget de descripción
-              SizedBox(height: 16), // Espacio entre secciones
-              VeterinaryServices(), // Llamas al widget de servicios
-              SizedBox(height: 16), // Espacio entre secciones
-              VeterinaryOpinions(), // Llamas al widget de opiniones
+            children: [
+              const CustomButtons(),
+              const SizedBox(height: 16),
+              GestureDetector(
+                onTap: () => Navigator.pushNamed(context, '/home'),
+                child: VetClinicCard(vetCenter: vetCenter!),
+              ),
+              const SizedBox(height: 16),
+              VeterinaryDescription(description: vetCenter!['description']),
+              const SizedBox(height: 16),
+              VeterinaryServices(services: vetCenter!['services']),
+              const SizedBox(height: 16),
+              const VeterinaryOpinions(),
             ],
           ),
         ),
