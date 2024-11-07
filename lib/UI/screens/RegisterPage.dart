f91306b7947047536a56ead66ce3683387b2aee6
RodrLH <u202212338@upc.edu.pe>
2024-11-07 15:50:43 -0500

    feat: add RegisterPage.dart

---
+++ lib/UI/screens/RegisterPage.dart
@@ -0,0 +1,288 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../widgets/CustomTextField.dart';
+import 'FinalConfigurationPage.dart';
+
+class RegisterPage extends StatefulWidget {
+  final Function(Map<String, dynamic>) onRegister;
+
+  const RegisterPage({super.key, required this.onRegister});
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
+  bool isClientSelected = true;
+
+  Future<void> _registerUser() async {
+    if (nameController.text.isEmpty ||
+        emailController.text.isEmpty ||
+        passwordController.text.isEmpty ||
+        confirmPasswordController.text.isEmpty ||
+        phoneController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill all fields')),
+      );
+      return;
+    }
+
+    if (passwordController.text != confirmPasswordController.text) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Passwords do not match')),
+      );
+      return;
+    }
+
+    final int roleId = isClientSelected ? 2 : 1;
+    final Map<String, dynamic> user = {
+      'id': 0,
+      'name': nameController.text,
+      'email': emailController.text,
+      'password': passwordController.text,
+      'phone': phoneController.text,
+      'roleId': roleId,
+    };
+
+    final response = await http.post(
+      Uri.parse('https://resilient-contentment-production.up.railway.app/api/users'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(user),
+    );
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      final responseData = json.decode(response.body);
+      final int userId = responseData['id'];
+      print('User Registered: ${user['name']}');
+      widget.onRegister(user);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FinalConfigurationPage(
+            userId: userId,
+            phone: user['phone'],
+            roleId: roleId,
+            name: user['name'],
+            email: user['email'],
+            password: user['password'],
+          ),
+        ),
+      );
+    } else {
+      print('Failed to register user');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xFF5CC6A9),
+        flexibleSpace: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 36,
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            color: const Color(0xFFFFF6E5),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset('assets/images/cat.png', height: 123),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF6BE69),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Registrarse',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0E5252),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Ingresa tus datos personales para crear tu cuenta en VetConnect',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFF000000),
+                          fontSize: 10,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  isClientSelected = true;
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: isClientSelected
+                                    ? const Color(0xFFA6FFE8)
+                                    : const Color(0xFF1E4E3C),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                'Cliente',
+                                style: TextStyle(
+                                    color: isClientSelected
+                                        ? const Color(0xFF000000)
+                                        : Colors.white),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  isClientSelected = false;
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: isClientSelected
+                                    ? const Color(0xFF1E4E3C)
+                                    : const Color(0xFFA6FFE8),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                'Empresa',
+                                style: TextStyle(
+                                    color: isClientSelected
+                                        ? Colors.white
+                                        : const Color(0xFF000000)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      CustomTextField(
+                          label: 'Name', controller: nameController),
+                      const SizedBox(height: 16),
+                      CustomTextField(
+                          label: 'Email', controller: emailController),
+                      const SizedBox(height: 16),
+                      CustomTextField(
+                          label: 'Phone', controller: phoneController),
+                      const SizedBox(height: 16),
+                      CustomTextField(
+                        label: 'Password',
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16),
+                      CustomTextField(
+                        label: 'Confirm Password',
+                        controller: confirmPasswordController,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 32),
+                      ElevatedButton(
+                        onPressed: _registerUser,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF67DBBE),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 25),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Text(
+                          'Crear cuenta',
+                          style: TextStyle(
+                            color: Color(0xFF193D30),
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('¿Ya tienes una cuenta? '),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/');
+                            },
+                            child: const Text(
+                              'Iniciar Sesión',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Color(0xFF193D30),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/images/pets.png', height: 200),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
